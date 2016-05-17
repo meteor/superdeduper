0aeb0cb7cd78f6d4c7be38d816d3bb65c23da7b9
Tom Coleman <tom@thesnail.org>
2016-05-17 10:55:28 -0700

    First (not really but kinda) npm solving

--- index.js
+++ index.js
@@ -1,40 +1,56 @@
 require('babel-register');
+var fs = require('fs');
 var _ = require('lodash');
 
-// require('./lib/fetch.js').then(metadata => {
-//   console.log(Object.keys(metadata));
-// });
+const dependencies = _.keys(require('./package.json').dependencies);
 
-var makeDep = function (constraint) {
-  return {
-    constraint: constraint,
-    arch: 'arch'
+// basically for testing whilst I'm on the plane
+const METADATA_CACHE = './metadata.cache.json';
+var promise;
+
+try {
+  promise = Promise.resolve(JSON.parse(fs.readFileSync(METADATA_CACHE)));
+} catch (e) {
+  promise = require('./lib/fetch.js').then(metadata => {
+    fs.writeFileSync(METADATA_CACHE, JSON.stringify(metadata));
+    return metadata;
+  });
+}
+
+promise.then(function(metadata) {
+  const catalog = {
+    getSortedVersionRecords: function(packageName) {
+      const versions = metadata[packageName].versions;
+      return _.map(versions, function(version) {
+        const dependencies = {};
+        _.each(version.dependencies, function(constraint, depName) {
+          dependencies[depName] = {
+            constraint: constraint,
+            references: [
+              { arch: "os" },
+              { arch: "web.browser"},
+              { arch: "web.cordova"}
+            ]
+          }
+        });
+        return {
+          packageName: packageName,
+          version: version.version,
+          dependencies: dependencies,
+        };
+      });
+    },
+    getVersion: function(packageName, version) {
+      return _.find(this.getSortedVersionRecords(packageName), function(d) { return d.version === version; });
+    }
   };
-};
 
-var packages = {
-  a: [
-    {packageName: 'a', version: '1.0.0', dependencies: {}},
-    {packageName: 'a', version: '2.0.0', dependencies: {}},
-  ],
-  b: [
-    {packageName: 'b', version: '1.0.0', dependencies: {a: makeDep('1.0.0')}},
-    {packageName: 'b', version: '2.0.0', dependencies: {a: makeDep('2.0.0')}}
-  ],
-  c: [
-    {packageName: 'c', version: '1.0.0', dependencies: {a: makeDep('1.0.0')}},
-  ]
-};
-
-const catalog = {
-  getSortedVersionRecords: function(packageName) {
-    return packages;
-  },
-  getVersion: function(packageName, version) {
-    return _.find(packages[packageName], function(d) { return d.version === version; });
+  var PackagesResolver = require('./lib/constraint-solver');
+  var r = new PackagesResolver(catalog, {});
+  try {
+    console.log(r.resolve(dependencies, [], {}));
+  } catch (err) {
+    console.log(err);
+    console.log(err.stack)
   }
-};
-
-var PackagesResolver = require('./lib/constraint-solver');
-var r = new PackagesResolver(catalog, {});
-console.log(r.resolve(['a', 'b', 'c'], [], {}));
+});
